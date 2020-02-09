dcc1813b55052d78f01fe14f013aac17a7b01cc1
R-t0923 <t.07r.12@gmail.com>
2020-02-09 06:10:31 +0000

    add favorites

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   root to: "home#top"
   get 'home/about' => 'home#about',as:"home_about"
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    # post 'favorites/index_create' => 'favorites#index_create',as:"favorite_create"
+    # delete 'favorites/index_destroy' => 'favorites#index_destroy',as:"favorite_destroy"
+  end
   
   
 end
