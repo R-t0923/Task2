327b99f6374900df7cb11b4a26785b313bf731f4
R-t0923 <t.07r.12@gmail.com>
2020-02-11 06:50:40 +0000

    Add search_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#top"
   get 'home/about' => 'home#about',as:"home_about"
-  resources :users,only: [:show,:index,:edit,:update] 
+  resources :users,only: [:show,:index,:edit,:update] do
+  get :folower
+  get 'folow' =>'users#folow'
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :relationships, only: [:create, :destroy]
-  
+  resources :relationships, only: [:index,:create, :destroy]
 end
 
